01921803c0778219a7c5c6d23c060befa48693b5
diogoazevedo15 <68329635+diogoazevedo15@users.noreply.github.com>
2024-12-30 12:59:48 +0000

    Simplify pipeline

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,2 @@
 # Log file name
-LOG_FILE_NAME = "validation_log.txt"
-
-# Validation success message
-VALIDATION_SUCCESS_MESSAGE = "No issues detected with the files"
+UTILS_CONST = "This is a CONSTANT from utils."
--- src/utils/prompts.py
+++ src/utils/prompts.py
@@ -1,5 +1,2 @@
 # Example prompt for an LLM
-DEFAULT_PROMPT = "Please process the following data: {data}"
-
-def generate_prompt(data):
-    return DEFAULT_PROMPT.format(data=data)
+UTILS_PROMPT = "This is a PROMPT from utils."
