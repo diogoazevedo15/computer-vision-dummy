14af33f1e840a1b492ce0b526d8a2d8ae4c413d7
diogoazevedo15 <68329635+diogoazevedo15@users.noreply.github.com>
2025-01-02 17:12:52 +0000

    update pipeline

--- src/components/cv_model_inference.py
+++ src/components/cv_model_inference.py
@@ -1,24 +1,26 @@
 def main():
     import argparse
+    from pathlib import Path
     from utils.functions import utils_fun
     from utils.constants import UTILS_CONST
     from utils.prompts import UTILS_PROMPT
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str, required=True)
-    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument('--output_url', type=str, required=True)
     args = parser.parse_args()
 
     input_dir = args.input_dir
     output_dir = args.output_dir
 
     print("Running model inference...")
-    print(f"Input data path: {input_dir}")
     print(UTILS_CONST)
     print(UTILS_PROMPT)
     utils_fun()
     print(f'Input dir: {input_dir}')
     print(f'Output dir: {output_dir}')
+
+    with open((Path(args.score_report) / "validate.txt"), "a") as f:
+        f.write("Inference step ran successfully")
 
 if __name__ == "__main__":
     main()
